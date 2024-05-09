e737a4ecde0a82a87a53bc98101accc77f178e5f
Mohammed <arrahmaan9901@gmail.com>
2024-05-09 19:56:45 +0530

    included only desired tests

--- scripts/train.sh
+++ scripts/train.sh
@@ -11,96 +11,7 @@ python3 train.py --config configs/default/mnist/dnn3.yaml
 python3 train.py --config configs/default/mnist/dnn5.yaml
 python3 train.py --config configs/default/mnist/dnn8.yaml
 
-# lincomb
-python3 train.py --config configs/lincomb/cifar10/resnet8.yaml
-python3 train.py --config configs/lincomb/cifar10/resnet14.yaml
-python3 train.py --config configs/lincomb/cifar10/resnet20.yaml
-python3 train.py --config configs/lincomb/cifar10/resnet26.yaml
-python3 train.py --config configs/lincomb/cifar10/resnet32.yaml
-python3 train.py --config configs/lincomb/cifar10/resnet44.yaml
-python3 train.py --config configs/lincomb/cifar10/resnet56.yaml
-python3 train.py --config configs/lincomb/mnist/dnn2.yaml
-python3 train.py --config configs/lincomb/mnist/dnn3.yaml
-python3 train.py --config configs/lincomb/mnist/dnn5.yaml
-python3 train.py --config configs/lincomb/mnist/dnn8.yaml
 
-# shilu
-python3 train.py --config configs/shilu/cifar10/resnet8.yaml
-python3 train.py --config configs/shilu/cifar10/resnet14.yaml
-python3 train.py --config configs/shilu/cifar10/resnet20.yaml
-python3 train.py --config configs/shilu/cifar10/resnet26.yaml
-python3 train.py --config configs/shilu/cifar10/resnet32.yaml
-python3 train.py --config configs/shilu/cifar10/resnet44.yaml
-python3 train.py --config configs/shilu/cifar10/resnet56.yaml
-python3 train.py --config configs/shilu/mnist/dnn2.yaml
-python3 train.py --config configs/shilu/mnist/dnn3.yaml
-python3 train.py --config configs/shilu/mnist/dnn5.yaml
-python3 train.py --config configs/shilu/mnist/dnn8.yaml
-
-# scaledsoftsign
-python3 train.py --config configs/scaledsoftsign/cifar10/resnet8.yaml
-python3 train.py --config configs/scaledsoftsign/cifar10/resnet14.yaml
-python3 train.py --config configs/scaledsoftsign/cifar10/resnet20.yaml
-python3 train.py --config configs/scaledsoftsign/cifar10/resnet26.yaml
-python3 train.py --config configs/scaledsoftsign/cifar10/resnet32.yaml
-python3 train.py --config configs/scaledsoftsign/cifar10/resnet44.yaml
-python3 train.py --config configs/scaledsoftsign/cifar10/resnet56.yaml
-python3 train.py --config configs/scaledsoftsign/mnist/dnn2.yaml
-python3 train.py --config configs/scaledsoftsign/mnist/dnn3.yaml
-python3 train.py --config configs/scaledsoftsign/mnist/dnn5.yaml
-python3 train.py --config configs/scaledsoftsign/mnist/dnn8.yaml
-
-# relun
-python3 train.py --config configs/relun/cifar10/resnet8.yaml
-python3 train.py --config configs/relun/cifar10/resnet14.yaml
-python3 train.py --config configs/relun/cifar10/resnet20.yaml
-python3 train.py --config configs/relun/cifar10/resnet26.yaml
-python3 train.py --config configs/relun/cifar10/resnet32.yaml
-python3 train.py --config configs/relun/cifar10/resnet44.yaml
-python3 train.py --config configs/relun/cifar10/resnet56.yaml
-python3 train.py --config configs/relun/mnist/dnn2.yaml
-python3 train.py --config configs/relun/mnist/dnn3.yaml
-python3 train.py --config configs/relun/mnist/dnn5.yaml
-python3 train.py --config configs/relun/mnist/dnn8.yaml
-
-# helu
-python3 train.py --config configs/helu/cifar10/resnet8.yaml
-python3 train.py --config configs/helu/cifar10/resnet14.yaml
-python3 train.py --config configs/helu/cifar10/resnet20.yaml
-python3 train.py --config configs/helu/cifar10/resnet26.yaml
-python3 train.py --config configs/helu/cifar10/resnet32.yaml
-python3 train.py --config configs/helu/cifar10/resnet44.yaml
-python3 train.py --config configs/helu/cifar10/resnet56.yaml
-python3 train.py --config configs/helu/mnist/dnn2.yaml
-python3 train.py --config configs/helu/mnist/dnn3.yaml
-python3 train.py --config configs/helu/mnist/dnn5.yaml
-python3 train.py --config configs/helu/mnist/dnn8.yaml
-
-# delu
-python3 train.py --config configs/delu/cifar10/resnet8.yaml
-python3 train.py --config configs/delu/cifar10/resnet14.yaml
-python3 train.py --config configs/delu/cifar10/resnet20.yaml
-python3 train.py --config configs/delu/cifar10/resnet26.yaml
-python3 train.py --config configs/delu/cifar10/resnet32.yaml
-python3 train.py --config configs/delu/cifar10/resnet44.yaml
-python3 train.py --config configs/delu/cifar10/resnet56.yaml
-python3 train.py --config configs/delu/mnist/dnn2.yaml
-python3 train.py --config configs/delu/mnist/dnn3.yaml
-python3 train.py --config configs/delu/mnist/dnn5.yaml
-python3 train.py --config configs/delu/mnist/dnn8.yaml
-
-# sinlu
-python3 train.py --config configs/sinlu/cifar10/resnet8.yaml
-python3 train.py --config configs/sinlu/cifar10/resnet14.yaml
-python3 train.py --config configs/sinlu/cifar10/resnet20.yaml
-python3 train.py --config configs/sinlu/cifar10/resnet26.yaml
-python3 train.py --config configs/sinlu/cifar10/resnet32.yaml
-python3 train.py --config configs/sinlu/cifar10/resnet44.yaml
-python3 train.py --config configs/sinlu/cifar10/resnet56.yaml
-python3 train.py --config configs/sinlu/mnist/dnn2.yaml
-python3 train.py --config configs/sinlu/mnist/dnn3.yaml
-python3 train.py --config configs/sinlu/mnist/dnn5.yaml
-python3 train.py --config configs/sinlu/mnist/dnn8.yaml
 
 # coslu
 python3 train.py --config configs/coslu/cifar10/resnet8.yaml
@@ -115,7 +26,7 @@ python3 train.py --config configs/coslu/mnist/dnn3.yaml
 python3 train.py --config configs/coslu/mnist/dnn5.yaml
 python3 train.py --config configs/coslu/mnist/dnn8.yaml
 
-# coslu
+# cossinlu
 python3 train.py --config configs/cossinlu/cifar10/resnet8.yaml
 python3 train.py --config configs/cossinlu/cifar10/resnet14.yaml
 python3 train.py --config configs/cossinlu/cifar10/resnet20.yaml
@@ -127,16 +38,3 @@ python3 train.py --config configs/cossinlu/mnist/dnn2.yaml
 python3 train.py --config configs/cossinlu/mnist/dnn3.yaml
 python3 train.py --config configs/cossinlu/mnist/dnn5.yaml
 python3 train.py --config configs/cossinlu/mnist/dnn8.yaml
-
-# normlincomb
-python3 train.py --config configs/normlincomb/cifar10/resnet8.yaml
-python3 train.py --config configs/normlincomb/cifar10/resnet14.yaml
-python3 train.py --config configs/normlincomb/cifar10/resnet20.yaml
-python3 train.py --config configs/normlincomb/cifar10/resnet26.yaml
-python3 train.py --config configs/normlincomb/cifar10/resnet32.yaml
-python3 train.py --config configs/normlincomb/cifar10/resnet44.yaml
-python3 train.py --config configs/normlincomb/cifar10/resnet56.yaml
-python3 train.py --config configs/normlincomb/mnist/dnn2.yaml
-python3 train.py --config configs/normlincomb/mnist/dnn3.yaml
-python3 train.py --config configs/normlincomb/mnist/dnn5.yaml
-python3 train.py --config configs/normlincomb/mnist/dnn8.yaml
