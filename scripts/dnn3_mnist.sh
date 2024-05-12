2b7b09e1a56d7e4683cef09f505bf8a4400dc662
Mohammed <arrahmaan9901@gmail.com>
2024-05-12 14:00:59 +0530

    updated dnn train to only include coslu cossinlu and default

--- scripts/dnn3_mnist.sh
+++ scripts/dnn3_mnist.sh
@@ -2,24 +2,24 @@
 python3 train.py --config configs/coslu/mnist/dnn3.yaml
 python3 train.py --config configs/cossinlu/mnist/dnn3.yaml
 python3 train.py --config configs/default/mnist/dnn3.yaml
-python3 train.py --config configs/delu/mnist/dnn3.yaml
-python3 train.py --config configs/helu/mnist/dnn3.yaml
-python3 train.py --config configs/lincomb/mnist/dnn3.yaml
-python3 train.py --config configs/normlincomb/mnist/dnn3.yaml
-python3 train.py --config configs/relun/mnist/dnn3.yaml
-python3 train.py --config configs/scaledsoftsign/mnist/dnn3.yaml
-python3 train.py --config configs/shilu/mnist/dnn3.yaml
-python3 train.py --config configs/sinlu/mnist/dnn3.yaml
+# python3 train.py --config configs/delu/mnist/dnn3.yaml
+# python3 train.py --config configs/helu/mnist/dnn3.yaml
+# python3 train.py --config configs/lincomb/mnist/dnn3.yaml
+# python3 train.py --config configs/normlincomb/mnist/dnn3.yaml
+# python3 train.py --config configs/relun/mnist/dnn3.yaml
+# python3 train.py --config configs/scaledsoftsign/mnist/dnn3.yaml
+# python3 train.py --config configs/shilu/mnist/dnn3.yaml
+# python3 train.py --config configs/sinlu/mnist/dnn3.yaml
 
 # test
 python3 test.py --config configs/coslu/mnist/dnn3.yaml
 python3 test.py --config configs/cossinlu/mnist/dnn3.yaml
 python3 test.py --config configs/default/mnist/dnn3.yaml
-python3 test.py --config configs/delu/mnist/dnn3.yaml
-python3 test.py --config configs/helu/mnist/dnn3.yaml
-python3 test.py --config configs/lincomb/mnist/dnn3.yaml
-python3 test.py --config configs/normlincomb/mnist/dnn3.yaml
-python3 test.py --config configs/relun/mnist/dnn3.yaml
-python3 test.py --config configs/scaledsoftsign/mnist/dnn3.yaml
-python3 test.py --config configs/shilu/mnist/dnn3.yaml
-python3 test.py --config configs/sinlu/mnist/dnn3.yaml
+# python3 test.py --config configs/delu/mnist/dnn3.yaml
+# python3 test.py --config configs/helu/mnist/dnn3.yaml
+# python3 test.py --config configs/lincomb/mnist/dnn3.yaml
+# python3 test.py --config configs/normlincomb/mnist/dnn3.yaml
+# python3 test.py --config configs/relun/mnist/dnn3.yaml
+# python3 test.py --config configs/scaledsoftsign/mnist/dnn3.yaml
+# python3 test.py --config configs/shilu/mnist/dnn3.yaml
+# python3 test.py --config configs/sinlu/mnist/dnn3.yaml
